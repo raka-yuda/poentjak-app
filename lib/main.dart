df2f6b6d01e6eb4c28cf7d7d01b389ef82a4bfaf
rakayuda <raka.yuda.pradipta@gmail.com>
2020-09-21 22:18:54 +0700

    implement search delegate

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:test_app/bloc/article_bloc.dart';
 import 'bloc/list_save_bloc.dart';
 import 'pages/home.dart';
 import 'pages/search.dart';
@@ -7,24 +8,45 @@ import 'pages/favorite.dart';
 
 void main() => runApp(Poentjak());
 
-class Poentjak extends StatelessWidget {
+class Poentjak extends StatefulWidget {
+  @override
+  _PoentjakState createState() => _PoentjakState();
+}
+
+class _PoentjakState extends State<Poentjak> {
+  final ListSaveBloc _listSaveBloc = ListSaveBloc();
+  final ArticleBloc _articleBloc = ArticleBloc();
+
+  @override
+  void initState() {
+    _articleBloc.add(ArticleEvent());
+    // _listSaveBloc.add(ListSaveEvent());
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<ListSaveBloc>(
-      create: (context) => ListSaveBloc(),
-      child: MaterialApp(
-        title: "Poentjak App",
-        theme: ThemeData(
-          fontFamily: 'google-font',
-        ),
-        initialRoute: Home.id,
-        routes: {
-          Home.id: (context) => Home(),
-          SearchPage.id: (context) => SearchPage(),
-          FavoritePage.id: (context) => FavoritePage(),
-        },
-        debugShowCheckedModeBanner: false,
-      ),
-    );
+    return MultiBlocProvider(
+        providers: [
+          BlocProvider<ListSaveBloc>(
+            create: (BuildContext context) => _listSaveBloc,
+          ),
+          BlocProvider<ArticleBloc>(
+            create: (BuildContext context) => _articleBloc,
+          ),
+        ],
+        child: MaterialApp(
+          title: "Poentjak App",
+          theme: ThemeData(
+            fontFamily: 'google-font',
+          ),
+          initialRoute: Home.id,
+          routes: {
+            Home.id: (context) => Home(),
+            SearchPage.id: (context) => SearchPage(),
+            FavoritePage.id: (context) => FavoritePage(),
+          },
+          debugShowCheckedModeBanner: false,
+        ));
   }
 }
--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -1,41 +1,148 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:test_app/assets/style.dart';
+import 'package:test_app/bloc/article_bloc.dart';
+import 'package:test_app/models/articles.dart';
+import 'package:test_app/widgets/article_widget.dart';
 
-class SearchPage extends StatelessWidget {
+class SearchPage extends StatefulWidget {
   static const String id = 'search';
 
   @override
+  _SearchPageState createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  List<String> listArticle = List<String>();
+  @override
   Widget build(BuildContext context) {
+    final ArticleBloc _articleBloc = BlocProvider.of<ArticleBloc>(context);
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text("Search Page"),
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back_ios,
+              size: 24,
+              color: darkBlue,
+            ),
+            onPressed: () => Navigator.pop(context),
+          ),
+          backgroundColor: Colors.white,
+          actions: <Widget>[
+            IconButton(
+                onPressed: () {
+                  showSearch(context: context, delegate: Search(listArticle));
+                },
+                icon: Icon(
+                  Icons.search,
+                  size: 24,
+                  color: darkBlue,
+                ))
+          ],
+          centerTitle: true,
+          title: Text(
+            'Search Article',
+            style: TextStyle(color: darkBlue),
+          ),
         ),
+        body: ListView(
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              padding: EdgeInsets.only(left: 16, right: 16),
+              child: BlocBuilder<ArticleBloc, ArticleState>(
+                  bloc: _articleBloc,
+                  builder: (context, state) {
+                    if (state is ArticleLoading) {
+                      return WidgetCircularLoading();
+                    } else if (state is ArticleFailure) {
+                      return Center(
+                        child: Text('${state.errorMessage}'),
+                      );
+                    } else if (state is ArticleLoaded) {
+                      Articles articles = state.articles;
+                      return ListView.builder(
+                          itemCount: articles.listArticles.length,
+                          itemBuilder: (context, index) {
+                            Article article = articles.listArticles[index];
+                            if (!listArticle.contains(article.titleArticle)) {
+                              listArticle.add(article.titleArticle);
+                            }
+                            return ListTile(
+                              title: Text(article.titleArticle),
+                            );
+                          });
+                    } else {
+                      return Container();
+                    }
+                  }),
+            )
+          ],
+        ));
+  }
+}
+
+class Search extends SearchDelegate {
+  final List<String> listItem;
+  List<String> recentList = ['The holiday', 'Plane Accident'];
+  String selectedItem;
+
+  Search(this.listItem);
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return <Widget>[
+      IconButton(
+        icon: Icon(
+          Icons.close,
+          size: 24,
+          color: darkBlue,
+        ),
+        onPressed: () => query = "",
+      )
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: Icon(
+        Icons.arrow_back_ios,
+        size: 24,
+        color: darkBlue,
+      ),
+      onPressed: () => Navigator.pop(context),
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text(selectedItem),
       ),
     );
   }
 
-  Widget searchBar() {
-    return Container(
-      height: 64,
-      padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(8)),
-          color: Colors.blueAccent[100],
-          border: Border.all(width: 2, color: Colors.blueAccent[200])),
-      child: Row(
-        children: <Widget>[
-          Text(
-            "Search...",
-            style: TextStyle(color: Colors.black26),
-          ),
-          Icon(
-            Icons.search,
-            color: Colors.black26,
-            size: 32,
-          )
-        ],
-      ),
-    );
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List<String> suggestionList = [];
+    query.isEmpty
+        ? suggestionList = recentList
+        : suggestionList
+            .addAll(listItem.where((element) => element.contains(query)));
+    return ListView.builder(
+        itemCount: suggestionList.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(suggestionList[index]),
+            onTap: () {
+              selectedItem = suggestionList[index];
+              showResults(context);
+            },
+          );
+        });
   }
 }
